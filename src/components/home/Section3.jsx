dc3d1470329544df2d3b34f814238b11873b82cf
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2026-03-06 17:24:59 +0530

    images

--- src/components/home/Section3.jsx
+++ src/components/home/Section3.jsx
@@ -7,10 +7,10 @@ const Section3 = () => {
   return (
     <>
    
-    <div className='section3' style={{backgroundImage: `url(${home4}`, backgroundSize: "cover", backgroundPosition: "center center", backgroundAttachment: "fixed"}}>
+    <div className='section3' style={{backgroundImage: `url(${home4}`, backgroundSize: "cover", backgroundPosition: "center center", backgroundAttachment: "fixed" }}>
    
        <div className="section-divider" style={{position:"relative", zIndex:2}}>
-  <img src={divider1} alt="divider" />
+  <img src={divider1} alt="divider"/>
 </div>
 
 
@@ -21,7 +21,7 @@ const Section3 = () => {
         <h2 className="cold-heading">Education from birth<br/>begins with us</h2>
         <p className="cold-subheading">Our children's academy, together with one of the oldest private schools, created a joint
           school preparation project. The goal of the project is the harmonious development, socializationfor admission
-          to the best educational institutions of our city.</p>
+          to the best educational institutions of ou dr city.</p>
       </div>
     </div>
     <div className="col-lg-6 col-md-6 col-sm-12 col-12"></div>
