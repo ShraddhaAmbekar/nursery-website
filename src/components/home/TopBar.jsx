24b199c311a8fd924e0a4db858f6baffb27cc372
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2026-03-20 10:32:23 +0530

    spelling check

--- src/components/home/TopBar.jsx
+++ src/components/home/TopBar.jsx
@@ -6,7 +6,7 @@ const TopBar = () => {
       <Container fluid className="d-flex justify-content-between">
         <div>
           <span className="link"><i className="fa fa-phone m-r5"></i>+91 9911255302</span>
-          <span className="ms-4 link"><i className="fa-solid fa-location-dot"></i> Sector 83, Gurgaon, Hryana</span>
+          <span className="ms-4 link"><i className="fa-solid fa-location-dot"></i> Sector 83, Gurgaon, Haryana</span>
         </div>
         <div>
           <span className="link"><i className="fa fa-clock-o m-r5"></i> Admission Open 2026:2027</span>
