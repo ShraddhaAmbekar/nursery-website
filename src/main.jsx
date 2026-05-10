a584ebd520ba81e7e0f4c3942f2f57b23ac9d278
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2026-05-10 16:42:17 +0530

    slick replaced by swiper

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import './App.css';
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
